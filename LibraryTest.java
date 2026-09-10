c836501ff13d601d4e6b94cce424f6c77e10e47a
Ibrahimalasady <Ibrahimalasady08@gmail.com>
2026-09-10 16:38:15 -0700

    Add unit tests for TinyLibrary functionality

--- LibraryTest.java
+++ LibraryTest.java
@@ -98,4 +98,32 @@ public class LibraryTest {
 	// -------------------------------------------------------------------------
 
 	// TO DO: Add more unit tests HERE to completely and robustly check the Library class.
+
+	@Test
+	public void testTinyLibraryLookupISBNCheckouut() {
+		tinyLibrary.checkOut(9780330351690L, 123, 10, 1, 2026);
+		assertEquals(123, tinyLibrary.lookup(9780330351690L));
+	}
+
+	@Test
+	public void testTinyLibraryCheckOutAgain() {
+		tinyLibrary.checkOut(9780330351690L, 123, 10, 1, 2026);
+		assertFalse(tinyLibrary.checkOut(9780330351690L, 456, 10, 5, 2026));
+	}
+
+	@Test
+	public void testTinyLibraryCheckInPatron() {
+		tinyLibrary.checkOut(9780330351690L, 123, 10, 1, 2026);
+		tinyLibrary.checkOut(9780446580342L, 123, 10, 1, 2026);
+		assertTrue(tinyLibrary.checkIn(123));
+		assertEquals(0, tinyLibrary.lookup(123).size());
+	}
+
+	@Test
+	public void testSmallLibraryLookup(){
+		smallLibrary.checkOut(9781843190004L, 999, 10, 15, 2026);
+		smallLibrary.checkOut(9781843190011L, 999, 10, 15, 2026);
+		ArrayList<LibraryBook> books = smallLibrary.lookup(999);
+		assertEquals(2, books.size());
+	}		
 }
