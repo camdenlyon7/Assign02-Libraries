43a9ab0456375e85c27b5fcdda5440aee4d953ac
Ibrahimalasady <Ibrahimalasady08@gmail.com>
2026-09-10 19:43:14 -0700

    Implement comparator for ordering books by author

--- OrderByAuthor.java
+++ OrderByAuthor.java
@@ -1,1 +1,17 @@
+package assign02;
 
+import java.util.Comparator;
+
+
+public classs OrderByAuthor<Type> implements Comparator<LibraryBookGeneric<Type>> {
+
+  @Override
+  public int compare(LibraryBookGeneric<Type> book1, LibraryBookGeneric<Type> book 2) {
+    int surnameCompare = book1.getAuthorSurname().compareTo(book2.getAuthorSurname());
+    if (surnameCompare != 0) {
+      return surnameCompare;
+    }
+
+    return book1.getTitle().compareTo(book2.getTitle());
+  }
+}
